caa268905548236bc3e12a79ebf155c15f219430
willlino <william@intranet.optcode.com.br>
2016-03-30 18:06:33 -0300

    Grid melhorada
    grid melhorada

--- Enviar/app/components/contact/contactController.js
+++ Enviar/app/components/contact/contactController.js
@@ -2,26 +2,128 @@
 	'use strict';
 
 	angular.module('app').controller('ContactController', function($scope, ContactRepository){
-		$scope.getContacts = getContacts();
+		$scope.contact = {
+		}
+		$scope.btnAddContactText = "Add Contact";
 
+		// If it is like this, it will execute when the controller is initialized
+		$scope.contacts = getContacts();
+
+		// If it is like this, it will execute only when the function is called
+		$scope.save = function(){
+			saveContact();
+		}
+
+		$scope.add = function(){
+			addContact();
+		}
+
+		$scope.edit = function(contact){
+			editContact(contact);
+		}
+
+		$scope.delete = function(contact){
+			deleteContact(contact);
+		}
+
+
+		
 		function getContacts(){
 			ContactRepository.get()
 				.then(function(data){
 					$scope.contacts = data.data;
+					//console.log(data.data);
 
 				}, function(error){
 					console.log(error);
+				});		
+		}
+
+		function saveContact(){
+			// If don't have an id, insert a new contact, else update
+			if($scope.contact.id == ''){
+				ContactRepository.post($scope.contact)
+				.then(function(data){
+					console.log(data);
+					$scope.contacts = getContacts();
+				}, function(error){
+					console.log(error);
 				});
-
-		function editContact(contact){
-
+			}else{
+				ContactRepository.put($scope.contact)
+				.then(function(data){
+					console.log(data);
+					$scope.contacts = getContacts();
+				}, function(error){
+					console.log(error);
+				});
+			}
 		}
 
 		function deleteContact(contact){
+			$scope.contact = {
+				id: contact.id,
+				name: contact.name,
+				email: contact.email,
+				phone: contact.phone,
+			    username: contact.username,
+				website: contact.website
+			};
 
+			ContactRepository.delete($scope.contact)
+				.then(function(data){
+					console.log(data);
+					$scope.contacts = getContacts();
+				}, function(error){
+					console.log(error);
+				});
 		}
 
+		// Show form to add contact
+		function addContact(){
+			$scope.contact = {
+				id: "",
+				name: "",
+				email: "",
+				phone: "",
+			    username: "",
+				website: ""
+			};
 
+			showHideContactForm();
+		}
+
+		// Show form to edit contact
+		function editContact(contact){
+			$scope.contact = {
+				id: contact.id,
+				name: contact.name,
+				email: contact.email,
+				phone: contact.phone,
+			    username: contact.username,
+				website: contact.website
+			};
+
+			$scope.contactFormVisible = true;
+			$scope.btnAddContactText = "Cancel";
+		}
+
+		function showHideContactForm(){
+			changeContactButtonVisibility();
+			changeContactButtonText();
+		}
+
+		function changeContactButtonText(){
+			var isVisible = $scope.contactFormVisible;
+
+			if(isVisible)
+				$scope.btnAddContactText = "Cancel";
+			else
+				$scope.btnAddContactText = "Add Contact";
+		}
+
+		function changeContactButtonVisibility(){
+			$scope.contactFormVisible = !$scope.contactFormVisible;
 		}
 	});
 })()
--- Enviar/app/components/contact/contactFactory.js
+++ Enviar/app/components/contact/contactFactory.js
@@ -2,31 +2,44 @@
 	'use strict';
 
 	angular.module('app').factory('ContactRepository', function($http){
-	var urlBase = 'http://jsonplaceholder.typicode.com/users';
-	var contact = {
-		email: "",
-		id: "",
-		name: ""};
-		var repository = {
+
+	var urlBase = 'http://jsonplaceholder.typicode.com/users/';
+
+	var repository = {
 		get: getContacts,
-		post: postContact(contact),
-		put: putContact(contact),
-		// delete: deleteContact(id)
-		};
-		
-		return repository;
-		
-		function getContacts(){
-			return $http.get(urlBase);
-		}
-		
-		function postContact(contact){
-			return $http.post(urlBase, contact);
- 		}
-		
-		function putContact(contact){
-		}
-		// function deleteContact(id){
-		// }
+		post: postContact,
+		put: putContact,
+		delete: deleteContact
+	}
+	return repository;
+
+
+	function getContacts(){
+		return $http.get(urlBase);
+	}
+
+	function postContact(contact){
+		return $http.post(urlBase, contact);
+    }
+
+	function putContact(contact){
+		return $http.put(urlBase + contact.id, contact);
+	}
+
+	function deleteContact(contact){
+		return $http.delete(urlBase + contact.id);
+	}
+
 	});
+
+	// function newContact(){
+	// 	var contact = {
+	// 	id: "",
+	// 	name: "",
+	// 	email: "",
+	// 	phone: "",
+	//     username: "",
+	// 	website: ""
+	// 	};
+	// }
 })();
